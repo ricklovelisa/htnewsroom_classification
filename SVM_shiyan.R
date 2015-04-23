af22e3500498d01ab665bf0e801b8af5b46498ec
QiuQiu <ricklovelisa@126.com>
2015-04-23 19:55:47 +0800

    整理训练集

--- SVM_shiyan.R
+++ SVM_shiyan.R
@@ -1,7 +1,23 @@
 library(RODBC)
+library(rJson)
 
-id <- read.table("电子商务文本ID", stringsAsFactors = F)
+
+id <- read.table("ecommerce_id.txt", stringsAsFactors = F)
 
 mycon <- odbcConnect("3.96", "root", "123456")
-data1 <- sqlQuery(mycon, paste("select id content_wordseg from article where id in (", id, ")", sep = ""), stringsAsFactors = F)
-data2 <- sqlQuery(mycon, "select article.id article.content_wordseg from article, article_classified where article.ID = article_classified.article_id and article_classified.category_id != 3 limit 3000")
+data1 <- sqlQuery(mycon, paste("select id, content_wordseg from article where id in (", id, ")", sep = ""), stringsAsFactors = F)
+data2 <- sqlQuery(mycon, "select article.id, article.content_wordseg from article limit 4000", stringsAsFactors = F)
+
+data2 <- data2[!(data2$id %in% data1$id), ]
+
+data <- rbind(data1, data2)
+rm(data1, data2)
+id <- as.integer(strsplit(id$V1, split = ",")[[1]])
+
+data$category <- 2
+data$category[data$id %in% id] <- 1
+
+rownames(data) <- paste(data$id, "_", data$category, sep = "")
+
+
+data.list() <- 
