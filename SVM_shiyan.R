eab51006d8be785d49c34ecf16a3a00769f1c820
Qiu Qiu <ricklovelisa@126.com>
2015-05-06 00:28:09 +0800

    更新程序和模型

--- SVM_shiyan.R
+++ SVM_shiyan.R
@@ -138,7 +138,7 @@ test.both <- DocumentTermMatrix(corpus.both, control.tf)
 
 # term_tfidf <- tapply(dtm.both$v/row_sums(dtm.both)[dtm.both$i], dtm.both$j, mean) * log2(nDocs(dtm.both)/col_sums(dtm.both > 0))
 # cont <- c((1:20)/200)
-cont <- c(50, 100, 150, 170, 190, 210, 230, 250, 260)
+cont <- c(170, 190, 210, 220, 230, 240, 250, 260)
 SVM_model <- list()
 SVM <- list()
 pred <- list()
